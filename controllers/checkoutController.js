92ee9f1105c96ea594c950fee08bf1909d5e3b24
martin <martinrevert@gmail.com>
2016-07-27 05:54:02 -0300

    - Fix diseño itemspedido - Fix carro de compras en Firefox - Agregado totales en carro de compra

--- controllers/checkoutController.js
+++ controllers/checkoutController.js
@@ -2,14 +2,20 @@
  * Created by martin on 29/06/16.
  */
 
-angular.module('app').controller('checkoutCtrl', function ($scope, cartSrv, enviarpedidoSrv) {
+angular.module('app').controller('checkoutCtrl', function ($scope, cartSrv, enviarpedidoSrv, $storage, $mdToast) {
 
-    console.log(cartSrv.getProducts());
+    var tablanotadeventa = $storage('tablanotadeventa');
 
     $scope.cart = cartSrv.getProducts();
 
-    $scope.eliminarItem = function () {
-        cartSrv.removeProduct();
+    $scope.direccionentrega = tablanotadeventa.getItem('direccionentrega');
+
+    $scope.total = cartSrv.getPrecioTotalDescuentoIVA();
+    $scope.preneto = cartSrv.getPrecioTotalDescuento();
+    $scope.iva = cartSrv.getIva();
+
+    $scope.eliminarItem = function (id) {
+        cartSrv.removeProduct(id);
         $scope.itemsummary = cartSrv.getItems();
         $scope.total = cartSrv.getPrecioTotalDescuentoIVA();
         $scope.preneto = cartSrv.getPrecioTotalDescuento();
@@ -26,10 +32,18 @@ angular.module('app').controller('checkoutCtrl', function ($scope, cartSrv, envi
     };
 
     $scope.checkout = function () {
+        //Todo introducir fechas en cart
         var pedido = cartSrv.getPedido();
+        enviarpedidoSrv.async(pedido).then(function () {
+            //envío de pedido exitoso
+            //Todo borrar array memoria
+            //Todo borrar tablanotadeventa
+            //Todo Toast avisando que el envío fue exitoso
 
-        enviarpedidoSrv.async(pedido);
+        }).then(function () {
+            //envío de pedido no exitoso
+            $mdToast.show($mdToast.simple().textContent('Falló el envío, por favor reintente.'));
+        });
     };
-
 
 });
